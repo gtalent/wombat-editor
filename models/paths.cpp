7457bb71498cd07ad8bd118910cd2af0e39ac1d9
Gary Talent <gtalent2@gmail.com>
2014-10-18 11:43:38 -0500

    Cleanup.

--- models/paths.cpp
+++ models/paths.cpp
@@ -4,14 +4,10 @@ namespace models {
 
 using cyborgbear::string;
 
-const cyborgbear::string Path_Animation = "Animation/";
-
-const cyborgbear::string Path_SpriteSheet = "Resources/SpriteSheets/";
-
-const string Path_TileClass = "World/TileClass/";
-
-const string Path_Zone = "World/Zone/";
-
-const string Path_ZoneHeader = "World/ZoneHeader/";
+const string Path_Animation   = "Animation/";
+const string Path_SpriteSheet = "Resources/SpriteSheets/";
+const string Path_TileClass   = "World/TileClass/";
+const string Path_Zone        = "World/Zone/";
+const string Path_ZoneHeader  = "World/ZoneHeader/";
 
 }
