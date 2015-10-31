9263c76a27d7558ffcf9be6cfc61da196b50cf92
Gary Talent <gtalent2@gmail.com>
2015-10-31 00:53:32 -0500

    Fixed SpriteSheets path.

--- models/paths.cpp
+++ models/paths.cpp
@@ -5,7 +5,7 @@ namespace models {
 using cyborgbear::string;
 
 const string Path_Animation   = "Animation/";
-const string Path_SpriteSheet = "Resources/SpriteSheets/";
+const string Path_SpriteSheet = "Resources/SpriteSheet/";
 const string Path_TileClass   = "World/TileClass/";
 const string Path_Zone        = "World/Zone/";
 const string Path_ZoneHeader  = "World/ZoneHeader/";
